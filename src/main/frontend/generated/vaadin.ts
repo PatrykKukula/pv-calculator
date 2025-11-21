4a3cb158eca7eda3f2b34e3d0750ee09567fd6bb
Asteez <kukla1416@wp.pl>
2025-11-22 00:30:55 +0100

    added ci-cd

--- src/main/frontend/generated/vaadin.ts
+++
@@ -1,8 +0,0 @@
-import './vaadin-featureflags.js';
-
-import './index';
-
-import './vaadin-react.js';
-import './theme-global-theme.global.generated.js';
-import { applyTheme } from './theme.js';
-applyTheme(document);
