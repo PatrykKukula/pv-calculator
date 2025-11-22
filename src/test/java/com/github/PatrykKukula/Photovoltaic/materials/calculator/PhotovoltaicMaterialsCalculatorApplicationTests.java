31b21e0f7228a51431e680c924cfcc7bd9fd1e29
Asteez <kukla1416@wp.pl>
2025-11-22 19:14:21 +0100

    added application-test.yml

--- src/test/java/com/github/PatrykKukula/Photovoltaic/materials/calculator/PhotovoltaicMaterialsCalculatorApplicationTests.java
+++ src/test/java/com/github/PatrykKukula/Photovoltaic/materials/calculator/PhotovoltaicMaterialsCalculatorApplicationTests.java
@@ -2,8 +2,10 @@ package com.github.PatrykKukula.Photovoltaic.materials.calculator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PhotovoltaicMaterialsCalculatorApplicationTests {
 
 	@Test
