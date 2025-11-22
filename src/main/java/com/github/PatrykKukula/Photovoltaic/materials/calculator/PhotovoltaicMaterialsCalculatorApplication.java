30970762a34df9a068d748c305d27673cf20b727
Asteez <kukla1416@wp.pl>
2025-11-22 18:36:57 +0100

    commit to test if CI/CD works fine

--- src/main/java/com/github/PatrykKukula/Photovoltaic/materials/calculator/PhotovoltaicMaterialsCalculatorApplication.java
+++ src/main/java/com/github/PatrykKukula/Photovoltaic/materials/calculator/PhotovoltaicMaterialsCalculatorApplication.java
@@ -13,5 +13,6 @@ public class PhotovoltaicMaterialsCalculatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PhotovoltaicMaterialsCalculatorApplication.class, args);
 	}
+	// CI/CD test
 
 }
