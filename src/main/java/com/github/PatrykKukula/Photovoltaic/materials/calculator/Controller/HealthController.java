bcf88106c07df6384cf5a4ad2cd966363a5436f5
Asteez <kukla1416@wp.pl>
2025-11-22 23:20:12 +0100

    /health endpoint update

--- src/main/java/com/github/PatrykKukula/Photovoltaic/materials/calculator/Controller/HealthController.java
+++ src/main/java/com/github/PatrykKukula/Photovoltaic/materials/calculator/Controller/HealthController.java
@@ -1,11 +1,12 @@
 package com.github.PatrykKukula.Photovoltaic.materials.calculator.Controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class HealthController {
+    @GetMapping("/health")
     public ResponseEntity<String> checkStatus(){
         return ResponseEntity.ok("ok");
     }
